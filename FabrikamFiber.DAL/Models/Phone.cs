bcef9a800e29bc4da5aa2f8d79d6a9b96c790006
Leo Lo <aslleolo@outlook.com>
2017-03-30 12:39:42 +0800

    Documents

--- FabrikamFiber.DAL/Models/Phone.cs
+++ FabrikamFiber.DAL/Models/Phone.cs
@@ -5,12 +5,16 @@
      */
     public class Phone
     {
+        /** ID for the phone number */
         public int ID { get; set; }
 
+        /** Label for the phone, should be human readable*/
         public string Label { get; set; }
 
+        /** The phone number itself */
         public string Number { get; set; }
 
+        /** The customer who own this phone number */
         public int CustomerID { get; set; }
 
 
